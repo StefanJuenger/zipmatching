87f5e4a4bd0699a07e3c453889696fca3c51577d
Jünger <stefan.juenger@gesis.org>
2024-10-29 11:54:46 +0100

    added patchwork dependency

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Required package names
-packages <- c("dplyr", "ggplot2", "sf", "tibble")
+packages <- c("dplyr", "ggplot2", "patchwork", "sf", "tibble")
 
 # Install missing packages
 install.packages(setdiff(packages, rownames(installed.packages())),
