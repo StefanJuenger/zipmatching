f927adab3f6acb65f8c883099817a87861ceaff6
juliandehne <julian.dehne@gmail.com>
2024-10-29 12:36:03 +0100

    Update install.R
    added missing packages to install.R

--- install.R
+++ install.R
@@ -1,7 +1,6 @@
-# Required package names
-packages <- c("dplyr", "ggplot2", "patchwork", "sf", "tibble")
-
-# Install missing packages
-install.packages(setdiff(packages, rownames(installed.packages())),
-                 dependencies = TRUE
-                 )
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("patchwork")
+install.packages("pbapply")
+install.packages("sf")
+install.packages("tibble")
